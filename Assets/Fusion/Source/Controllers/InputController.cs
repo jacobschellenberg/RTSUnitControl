b9c75831cca475be74bf212e360f8852fc6f2b2a
Jacob Schellenberg <jacob@schellex.com>
2016-12-11 02:16:25 -0700

    Implemented drag selection box. Reduced time till click becomes drag.

--- Assets/Fusion/Source/Controllers/InputController.cs
+++ Assets/Fusion/Source/Controllers/InputController.cs
@@ -17,19 +17,34 @@ public class InputController : MonoBehaviour {
 	}
 
 	[SerializeField] Camera mainCamera;
-	[SerializeField] float timeUntilMouseDrag = 0.004f; // if mouse down helder longer than timer, it's a drag.
+	[SerializeField] float timeUntilMouseDrag = 0.002f; // if mouse down helder longer than timer, it's a drag.
+	[SerializeField] RectTransform selectionBox;
 
 	float mouseDownTimer;
 	bool isMouseDown;
 
+	// This variable will store the location of wherever we first click before dragging.
+	private Vector2 initialClickPosition = Vector2.zero;
+
 	void Start() {
 		if (mainCamera == null && Camera.main != null)
 			mainCamera = Camera.main;
+
+		if (selectionBox == null)
+			LogController.Log ("Assign a dragbox to the InputController.");
 	}
 
 	void Update() {
-		if (Input.GetMouseButtonDown (0))
+		if (Input.GetMouseButtonDown (0)) {
+			// Get the initial click position of the mouse. No need to convert to GUI space
+			// since we are using the lower left as anchor and pivot.
+			initialClickPosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+
+			// The anchor is set to the same place.
+			selectionBox.anchoredPosition = initialClickPosition;
+
 			isMouseDown = true;
+		}
 
 		if (Input.GetMouseButtonUp (0)) {
 			if (mouseDownTimer < timeUntilMouseDrag)
@@ -45,7 +60,7 @@ public class InputController : MonoBehaviour {
 			mouseDownTimer += 0.01f * Time.deltaTime;
 
 		if (mouseDownTimer >= timeUntilMouseDrag)
-			MouseDrag ();
+			MouseDrag (Input.mousePosition);
 	}
 
 	public RaycastHitInfo MouseDown(Vector3 mousePosition) {
@@ -58,15 +73,47 @@ public class InputController : MonoBehaviour {
 		}
 
 		EventController.Publish("MouseDown", raycastHitInfo);
-		LogController.Log ("Mouse Down");
+		LogController.Log ("Mouse Down: " + mousePosition);
 		return raycastHitInfo;
 	}
 
 	public void MouseUp(Vector3 mousePosition) {
-		LogController.Log ("Mouse Up");
+		// Reset
+		initialClickPosition = Vector2.zero;
+		selectionBox.anchoredPosition = Vector2.zero;
+		selectionBox.sizeDelta = Vector2.zero;
+
+		LogController.Log ("Mouse Up: " + mousePosition);
 	}
 
-	public void MouseDrag() {
-		LogController.Log ("Mouse Drag");
+	public void MouseDrag(Vector3 position) {
+		// Store the current mouse position in screen space.
+		Vector2 currentMousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+
+		// How far have we moved the mouse?
+		Vector2 difference = currentMousePosition - initialClickPosition;
+
+		// Copy the initial click position to a new variable. Using the original variable will cause
+		// the anchor to move around to wherever the current mouse position is,
+		// which isn't desirable.
+		Vector2 startPoint = initialClickPosition;
+
+		// The following code accounts for dragging in various directions.
+		if (difference.x < 0)
+		{
+			startPoint.x = currentMousePosition.x;
+			difference.x = -difference.x;
+		}
+		if (difference.y < 0)
+		{
+			startPoint.y = currentMousePosition.y;
+			difference.y = -difference.y;
+		}
+
+		// Set the anchor, width and height every frame.
+		selectionBox.anchoredPosition = startPoint;
+		selectionBox.sizeDelta = difference;
+
+		LogController.Log ("Mouse Drag: " + position);
 	}
 }
