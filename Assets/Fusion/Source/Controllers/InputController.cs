b7279f32eab0980969474f08dbfa1850926c5206
Jacob Schellenberg <jacob@schellex.com>
2016-12-11 20:13:10 -0700

    Cleanup/formatting.

--- Assets/Fusion/Source/Controllers/InputController.cs
+++ Assets/Fusion/Source/Controllers/InputController.cs
@@ -4,7 +4,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 
-public enum InputEvent {
+public enum InputEvent
+{
 	LeftMouseButtonDown,
 	LeftMouseButtonUp,
 	LeftMouseButtonDrag,
@@ -12,17 +13,12 @@ public enum InputEvent {
 	RightMouseButtonUp
 }
 
-public class InputController : MonoBehaviour {
+public class InputController : MonoBehaviour
+{
 
 	[SerializeField] Camera mainCamera;
-	[SerializeField] float timeUntilMouseDrag = 0.002f; // if mouse down helder longer than timer, it's a drag.
-
-	int leftMouseButton = 0;
-	int rightMouseButton = 1;
-
-	float mouseDownTimer;
-	bool isMouseDown;
-	bool mouseUpFromDrag;
+	[SerializeField] float timeUntilMouseDrag = 0.002f;
+	// if mouse down helder longer than timer, it's a drag.
 
 	// Draggable inspector reference to the Image GameObject's RectTransform.
 	[SerializeField] RectTransform selectionBox;
@@ -31,25 +27,35 @@ public class InputController : MonoBehaviour {
 	// This variable will store the location of wherever we first click before dragging.
 	private Vector2 initialClickPosition = Vector2.zero;
 
+	private int leftMouseButton = 0;
+	private int rightMouseButton = 1;
 
-	void Start() {
+	private float mouseDownTimer;
+	private bool isMouseDown;
+	private bool mouseUpFromDrag;
+
+	private void Start ()
+	{
 		if (mainCamera == null && Camera.main != null)
 			mainCamera = Camera.main;
 
 		if (selectionBox == null)
-			LogController.Log ("Assign a dragbox to the InputController.");
+			LogController.Log ("Assign a selection box to the InputController.");
+
+		if (debugSelectionBox == null)
+			LogController.Log ("Assign a debug selection box to the InputController.");
 	}
 
-	void Update() {
+	private void Update ()
+	{
 		#region Left mouse button
 		if (Input.GetMouseButtonDown (leftMouseButton)) {
 			// Get the initial click position of the mouse. No need to convert to GUI space
 			// since we are using the lower left as anchor and pivot.
-			initialClickPosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+			initialClickPosition = new Vector2 (Input.mousePosition.x, Input.mousePosition.y);
 
 			// The anchor is set to the same place.
 			selectionBox.anchoredPosition = initialClickPosition;
-
 			isMouseDown = true;
 		}
 
@@ -57,7 +63,7 @@ public class InputController : MonoBehaviour {
 			if (mouseDownTimer < timeUntilMouseDrag)
 				LeftMouseButtonDown (Input.mousePosition);
 
-			LeftMouseButtonUp(Input.mousePosition);
+			LeftMouseButtonUp (Input.mousePosition);
 
 			isMouseDown = false;
 			mouseDownTimer = 0;
@@ -71,29 +77,28 @@ public class InputController : MonoBehaviour {
 		#endregion
 
 		#region Right mouse button
-		if(Input.GetMouseButtonDown(rightMouseButton))
-			RightMouseButtonDown(Input.mousePosition);
+		if (Input.GetMouseButtonDown (rightMouseButton))
+			RightMouseButtonDown (Input.mousePosition);
 
-		if(Input.GetMouseButtonUp(rightMouseButton))
-			RightMouseButtonUp(Input.mousePosition);
+		if (Input.GetMouseButtonUp (rightMouseButton))
+			RightMouseButtonUp (Input.mousePosition);
 		#endregion
 	}
 
-	private RaycastHitInfo GetRaycastHitInfo(Vector3 mousePosition)
+	private RaycastHitInfo GetRaycastHitInfo (Vector3 mousePosition)
 	{
 		RaycastHitInfo raycastHitInfo = null;
-		var ray = mainCamera.ScreenPointToRay(mousePosition);
+		var ray = mainCamera.ScreenPointToRay (mousePosition);
 		RaycastHit hitInfo;
 
 		if (Physics.Raycast (ray, out hitInfo, Mathf.Infinity))
 			raycastHitInfo = new RaycastHitInfo (hitInfo);
 
-
 		LogController.Log ("Raycast hit point: " + hitInfo.point);
 		return raycastHitInfo;
 	}
 
-	private List<RaycastHitInfo> GetRaycastHitInfos(Vector3 center, Vector3 size)
+	private List<RaycastHitInfo> GetRaycastHitInfos (Vector3 center, Vector3 size)
 	{
 		var raycastHitInfos = new List<RaycastHitInfo> ();
 
@@ -108,15 +113,17 @@ public class InputController : MonoBehaviour {
 		return raycastHitInfos;
 	}
 
-	private void LeftMouseButtonDown(Vector3 mousePosition) {
+	private void LeftMouseButtonDown (Vector3 mousePosition)
+	{
 		var raycastHitInfo = GetRaycastHitInfo (mousePosition);
 
-		EventController.Publish(InputEvent.LeftMouseButtonDown.ToString(), raycastHitInfo);
+		EventController.Publish (InputEvent.LeftMouseButtonDown.ToString (), raycastHitInfo);
 		LogController.Log ("Left mouse button down: " + mousePosition);
 	}
 
-	private void LeftMouseButtonUp(Vector2 mousePosition) {
-		var raycastHitInfos = new List<RaycastHitInfo>();
+	private void LeftMouseButtonUp (Vector2 mousePosition)
+	{
+		var raycastHitInfos = new List<RaycastHitInfo> ();
 
 		if (mouseUpFromDrag) {
 			var startPoint = GetRaycastHitInfo (initialClickPosition);
@@ -133,7 +140,7 @@ public class InputController : MonoBehaviour {
 			LogController.Log ("x1: " + x1 + " x2: " + x2);
 			LogController.Log ("y1: " + y1 + " y2: " + y2);
 
-			var size = new Vector3(Mathf.Abs(startPoint.Point.x - lastPoint.Point.x), 1, Mathf.Abs(startPoint.Point.z - lastPoint.Point.z));
+			var size = new Vector3 (Mathf.Abs (startPoint.Point.x - lastPoint.Point.x), 1, Mathf.Abs (startPoint.Point.z - lastPoint.Point.z));
 
 			LogController.Log ("Start point: " + startPoint.Point);
 			LogController.Log ("Last point: " + lastPoint.Point);
@@ -144,9 +151,9 @@ public class InputController : MonoBehaviour {
 			debugSelectionBox.position = midPoint;
 
 			raycastHitInfos = GetRaycastHitInfos (midPoint, size / 2);
-		}
+		} 
 		else
-			raycastHitInfos.Add(GetRaycastHitInfo (mousePosition));
+			raycastHitInfos.Add (GetRaycastHitInfo (mousePosition));
 
 		mouseUpFromDrag = false; // reset for next mouse up event
 
@@ -155,15 +162,16 @@ public class InputController : MonoBehaviour {
 		selectionBox.anchoredPosition = Vector2.zero;
 		selectionBox.sizeDelta = Vector2.zero;
 
-		EventController.Publish(InputEvent.LeftMouseButtonUp.ToString(), raycastHitInfos);
+		EventController.Publish (InputEvent.LeftMouseButtonUp.ToString (), raycastHitInfos);
 		LogController.Log ("Left mouse button up: " + mousePosition);
 	}
 
-	private void LeftMouseButtonDrag(Vector3 mousePosition) {
+	private void LeftMouseButtonDrag (Vector3 mousePosition)
+	{
 		mouseUpFromDrag = true;
 
 		// Store the current mouse position in screen space.
-		Vector2 currentMousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+		Vector2 currentMousePosition = new Vector2 (Input.mousePosition.x, Input.mousePosition.y);
 
 		// How far have we moved the mouse?
 		Vector2 difference = currentMousePosition - initialClickPosition;
@@ -174,13 +182,12 @@ public class InputController : MonoBehaviour {
 		Vector2 startPoint = initialClickPosition;
 
 		// The following code accounts for dragging in various directions.
-		if (difference.x < 0)
-		{
+		if (difference.x < 0) {
 			startPoint.x = currentMousePosition.x;
 			difference.x = -difference.x;
 		}
-		if (difference.y < 0)
-		{
+
+		if (difference.y < 0) {
 			startPoint.y = currentMousePosition.y;
 			difference.y = -difference.y;
 		}
@@ -193,17 +200,19 @@ public class InputController : MonoBehaviour {
 		LogController.Log ("Left mouse button drag: " + mousePosition);
 	}
 
-	private void RightMouseButtonDown(Vector3 mousePosition) {
+	private void RightMouseButtonDown (Vector3 mousePosition)
+	{
 		var raycastHitInfo = GetRaycastHitInfo (mousePosition);
 
-		EventController.Publish(InputEvent.RightMouseButtonDown.ToString(), raycastHitInfo);
+		EventController.Publish (InputEvent.RightMouseButtonDown.ToString (), raycastHitInfo);
 		LogController.Log ("Right mouse button down: " + mousePosition);
 	}
 
-	private void RightMouseButtonUp(Vector3 mousePosition) {
+	private void RightMouseButtonUp (Vector3 mousePosition)
+	{
 		var raycastHitInfo = GetRaycastHitInfo (mousePosition);
 
-		EventController.Publish(InputEvent.RightMouseButtonUp.ToString(), raycastHitInfo);
+		EventController.Publish (InputEvent.RightMouseButtonUp.ToString (), raycastHitInfo);
 		LogController.Log ("Right mouse button up: " + mousePosition);
 	}
 }
